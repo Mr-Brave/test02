183934c4f93945d87c8c49788e9d55471bd9da98
Mr-Brave <969094039@qq.com>
2017-12-04 19:52:45 +0800

    初始化项目

---
+++ src/main/java/User.java
@@ -0,0 +1,24 @@
+/**
+ * Created by superUser on 2017/12/4.
+ */
+public class User {
+
+    private  String  userName;
+    private  String userPwd;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPwd() {
+        return userPwd;
+    }
+
+    public void setUserPwd(String userPwd) {
+        this.userPwd = userPwd;
+    }
+}
